a160ca17ca7b1397f3d691f6c75e8ede4c84fe1e
genanddevid <genanddevid@gmail.com>
2025-09-07 22:29:37 +0100

    settings

--- genbugelproject/settings.py
+++ genbugelproject/settings.py
@@ -152,8 +152,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-#MEDIA_ROOT = "/opt/render/project/src/media"
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/opt/render/project/src/media"
 
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"

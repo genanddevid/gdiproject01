90c9f716c1b82263d5eded4fa42a5c52d9e2e98d
genanddevid <genanddevid@gmail.com>
2025-11-11 14:10:06 +0100

    changing back from DigitalOcean domain to ALLOWED_HOSTS

--- genbugelproject/settings.py
+++ genbugelproject/settings.py
@@ -33,12 +33,8 @@ DEBUG = True
 
 import os
 
-#ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'genbugel-8ef94.ondigitalocean.app',
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'genbugel-8ef94.ondigitalocean.app',]
 
 
 

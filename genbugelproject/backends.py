507ccb5d8e9e0b2cc7f13fe6406f421885094ced
genanddevid <genanddevid@gmail.com>
2026-05-01 13:50:39 +0100

    re doing the fix: login page extends base, correct redirect URLs, no 500 error

--- genbugelproject/backends.py
+++ genbugelproject/backends.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+
 class EmailOrUsernameBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
+        if not username or not password:
+            return None
         # Try email first
         try:
             user = User.objects.get(email=username)
@@ -9,6 +15,8 @@ class EmailOrUsernameBackend(ModelBackend):
                 user = User.objects.get(username=username)
             except User.DoesNotExist:
                 return None
+        except Exception:
+            return None
 
         if user.check_password(password) and self.user_can_authenticate(user):
             return user

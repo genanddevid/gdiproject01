38c193bbffa6e28c8980c65ad22eb01b691da70f
genanddevid <genanddevid@gmail.com>
2026-05-11 22:29:22 +0100

    fix: register SemanticTag in Django admin

--- post/admin.py
+++ post/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from post.models import Post, Tag, Follow, Stream
-# Register your models here.
+from post.models import Post, Tag, Follow, Stream, SemanticTag
+
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Follow)
 admin.site.register(Stream)
+
+@admin.register(SemanticTag)
+class SemanticTagAdmin(admin.ModelAdmin):
+    list_display = ('post', 'entity', 'category', 'parent_category', 'grandparent_category', 'created_at')
+    list_filter = ('category', 'parent_category', 'grandparent_category')
+    search_fields = ('entity', 'post__caption')

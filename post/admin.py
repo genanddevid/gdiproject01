17b92de96fb4e4b157881f989b83e7e3d202951b
genanddevid <genanddevid@gmail.com>
2026-05-12 11:12:18 +0100

    fix: remove orphaned semantictag migration and admin import

--- post/admin.py
+++ post/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from post.models import Post, Tag, Follow, Stream, SemanticTag
-
+from post.models import Post, Tag, Follow, Stream
+# Register your models here.
 admin.site.register(Post)
 admin.site.register(Tag)
 admin.site.register(Follow)
 admin.site.register(Stream)
-
-@admin.register(SemanticTag)
-class SemanticTagAdmin(admin.ModelAdmin):
-    list_display = ('post', 'entity', 'category', 'parent_category', 'grandparent_category', 'created_at')
-    list_filter = ('category', 'parent_category', 'grandparent_category')
-    search_fields = ('entity', 'post__caption')

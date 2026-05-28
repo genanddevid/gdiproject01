ae759515be9272d11c1c4713a9739abe3ea3f3f2
genanddevid <genanddevid@gmail.com>
2026-05-28 16:58:09 +0100

    fix: skipping R2

--- post/views.py
+++ post/views.py
@@ -543,10 +543,8 @@ def preview_narrative(request, post_id=None):
             temp_image_url = None
             if 'picture' in request.FILES:
                 picture_file = request.FILES['picture']
-                unique_filename = f"temp/{uuid.uuid4()}_{picture_file.name}"
-                saved_path = default_storage.save(unique_filename, ContentFile(picture_file.read()))
-                temp_image_url = default_storage.url(saved_path)
-                request.session['preview_data']['picture'] = saved_path
+                request.session['preview_data']['picture'] = None
+                temp_image_url = '/static/img/narrative-icon.png'
             elif instance and instance.picture:
                 temp_image_url = instance.picture.url    
 

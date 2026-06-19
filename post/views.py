c45cfd2a9e8f32b760ba4ac18991ff22088a8803
genanddevid <genanddevid@gmail.com>
2026-06-19 17:08:09 +0100

    fix: wordreference.com noun, etd

--- post/views.py
+++ post/views.py
@@ -1277,9 +1277,7 @@ def wordreference_lookup(request):
 
         return JsonResponse({
             'definition': definition,
-            'url': url,
-            'found_rh_def': soup.select_one('span.rh_def') is not None,
-            'html_sample': response.text[:2000]
+            'partOfSpeech': part_of_speech
         })
 
     except Exception as e:
